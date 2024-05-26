f294c0cbbcea4ab6111879359a13c6900b20b086
sxuu0072 <sxuu0072@student.monash.edu>
2024-05-26 10:44:29 +1000

    upload assessment 3 fit 1050 by Silin Xu 33048126
    upload assessment 3 fit 1050 by Silin Xu 33048126

---
+++ 1050ass3/layout/scripts/script.js
@@ -0,0 +1,39 @@
+
+//Section 1:Navigation
+
+// add classes for navigation toggling
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+
+
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+//nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('cs-active');
+	};
+	item.addEventListener('click', onClick);
+}
+
+function showMessage() {
+    alert('JavaScript!');
+}
+
+//Section 3: Gallery
+//apply to fancybox
+document.querySelector( 'h1' ).addEventListener( 'click', showMessage );
+Fancybox.bind( "[data-fancybox]" );        
+
